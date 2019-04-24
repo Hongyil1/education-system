e0971f41effc3c903d1b014d014d532c894d4b45
Hongyil1 <hylxmu@163.com>
2019-04-24 17:01:32 +1000

    try to figure out get and post

--- edusystem/resources/views/test.blade.php
+++ edusystem/resources/views/test.blade.php
@@ -1,1 +1,23 @@
-<h1>This is the test!!</h1>
+@extends('layout.app')
+
+@section('section')
+  <h1>This is the test!!</h1>
+
+  <form class="" action="index.html" method="post">
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label for="inputName">Name</label>
+        <input type="text" class="form-control" id="inputName">
+      </div>
+      <div class="form-group col-md-6">
+        <label for="inputCity">Age</label>
+        <input type="text" class="form-control" id="inputAge">
+      </div>
+    </div>
+    <div class="group">
+
+    </div>
+  </form>
+
+  <input class="btn btn-primary" type="submit" value="Submit">
+@endsection
